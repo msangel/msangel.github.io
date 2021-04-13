182870d6e466dbbf97943f68059da3a1a9bc19be
Vasyl Khrystiuk <h6.msangel@gmail.com>
2021-04-13 03:53:06 +0300

    link to sources

--- assets/links.js
+++ assets/links.js
@@ -2,7 +2,7 @@
 function external_new_window() {
     for(var c = document.getElementsByTagName("a"), a = 0;a < c.length;a++) {
         var b = c[a];
-        if(b.getAttribute("href") && b.hostname !== location.hostname) {
+        if(b.getAttribute("href") && !b.getAttribute("target") && b.hostname !== location.hostname) {
             b.target = "_blank";
             b.rel = "noopener";
         }
