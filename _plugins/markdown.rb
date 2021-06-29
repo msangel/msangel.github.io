9d2c8e8b480362cae93b8f53b52af0ab20766c18
Vasyl Khrystiuk <h6.msangel@gmail.com>
2021-06-30 01:39:20 +0300

    fix image prettier

--- _plugins/markdown.rb
+++ _plugins/markdown.rb
@@ -2,9 +2,7 @@ class Kramdown::Converter::Html
   # You can override any function from this page:
   # https://kramdown.gettalong.org/rdoc/Kramdown/Converter/Html.html
   def convert_img(el, _indent)
-    pp el;
-    throw 1;
-    if el.options.dig(:ial, :refs).include? 'pretty'
+    if el.options.dig(:ial, :refs)&.include? 'pretty'
       el.attr['class'] = "#{el.attr['class']} col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2".lstrip
       "<div class=\"row\">" \
         "<img#{html_attributes(el.attr)} />" \
